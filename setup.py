cee11676d24c62ebb5c7babff827314d02ccb82e
Poul Hornsleth <poulh@umich.edu>
2023-07-30 09:41:07 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='embeddings',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Poul Hornsleth',
+    author_email='',
+    description='',
+    install_requires=[
+        'openai',
+        'python-dotenv',
+        'langchain',
+        'streamlit',
+        'PyPDF2',
+        'watchdog',
+        'transformers',
+        'torch',
+        'InstructorEmbedding',
+        'sentence_transformers',
+        'faiss-cpu'
+
+       # 'htmlTemplates',
+    ]
+)
